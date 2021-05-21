89e671e096e609431ba5ccbeecb17ab24f8fb0cb
javiersandes <san20041@byui.edu>
2021-05-20 22:47:43 -0300

    Javier soy yo

--- src/byui/jrojas/Main.java
+++ src/byui/jrojas/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Hello, my name is Jonathan");
+        System.out.print( "Hello Jonathan, my name is Javier.");
     }
 }
