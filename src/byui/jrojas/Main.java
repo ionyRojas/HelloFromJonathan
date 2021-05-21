532c039fc63524f6658c49ce17902eaed7fd70e4
Cgualco <cameron.gualco@gmail.com>
2021-05-20 20:56:24 -0600

    Changed to println instead of print Added a line

--- src/byui/jrojas/Main.java
+++ src/byui/jrojas/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Hello, my name is Jonathan");
-        System.out.print( "Hello Jonathan, my name is Javier.");
+        System.out.println("Hello, my name is Jonathan");
+        System.out.println( "Hello Jonathan, my name is Javier.");
+        System.out.println("Hello Jonathan and Javier, My name is Cameron");
     }
 }
